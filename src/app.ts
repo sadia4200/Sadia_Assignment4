109098edcefd70a7747998f12778b50d3e0901d4
sadia4200 <sadia4200@users.noreply.github.com>
2026-07-08 18:51:43 +0600

    feat: add catch-all 404 handler for unmatched routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import apiRoutes from "./routes";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { AppError } from "./utils/errors";
 
 const app: Application = express();
 
@@ -22,6 +23,11 @@ app.get("/api/health", (_req: Request, res: Response) => {
 
 // --- API Routes ---
 app.use("/api", apiRoutes);
+
+// --- Catch-All 404 Handler for Unmatched Routes ---
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+  next(new AppError(404, "Route not found"));
+});
 
 // --- Global Error Handler ---
 app.use(errorHandler);
