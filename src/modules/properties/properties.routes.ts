025504cceb212b8794747996ec26d41207d270f6
sadia4200 <sadia4200@users.noreply.github.com>
2026-07-09 00:11:29 +0600

    feat: implement public GET reviews for a property

--- src/modules/properties/properties.routes.ts
+++ src/modules/properties/properties.routes.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import * as propertiesController from "./properties.controller";
 import { getPropertiesSchema } from "./properties.validation";
 import { validate } from "../../middlewares/validate.middleware";
+import * as reviewsController from "../reviews/reviews.controller";
+import { getPropertyReviewsSchema } from "../reviews/reviews.validation";
 
 const router = Router();
 
 router.get("/", validate(getPropertiesSchema), propertiesController.getProperties);
 router.get("/:id", propertiesController.getProperty);
+router.get("/:id/reviews", validate(getPropertyReviewsSchema), reviewsController.getPropertyReviews);
 
 export const propertiesRoutes = router;
